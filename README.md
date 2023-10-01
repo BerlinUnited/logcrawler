952b2be7161b58382059490fb7f710f0dc5b0b7c
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2023-10-01 21:36:13 +0200

    add code that connects to postgres and gets the version

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Berlin United LogCrawler
 
 TODO
+
+## Possible improvements
+- move the k8s yaml from the k8s cluster repo to this repo and use configmaps for the code
--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 """
 import pathlib
 import time
+import psycopg2
+
+params = {"host": "postgres.postgres","port": 5432,"dbname": "postgres","user": "postgres","password": "123"}
+conn = psycopg2.connect(**params)
+cur = conn.cursor()
+
+cur.execute("SELECT version()")
+version = cur.fetchone()
+print(f"Postgres version: {version}")
 
 # folder path
 dir_path = '/mnt/repl/'
