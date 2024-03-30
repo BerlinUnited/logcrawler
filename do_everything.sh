602e7704ed91738245305e7b98eeecf96b3946ef
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-03-30 18:28:09 +0100

    rename folder

--- do_everything.sh
+++ do_everything.sh
@@ -1,5 +1,5 @@
 python /scripts/db_ingester/main.py
-python /scripts/combine_images/main.py
+python /scripts/combine_logs/main.py
 python /scripts/representation_exporter/main.py
 python /scripts/check_images/main.py
 python /scripts/image_extractor/main.py
