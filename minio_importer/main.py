658d62c7ed979ddfb9ecb9175c79d01f9d17e267
Stella Alice Schlotter <stella.schlotter@deepl.com>
2024-04-14 17:37:04 +0200

    WIP try to deal with postgres errors

--- minio_importer/main.py
+++ minio_importer/main.py
@@ -3,6 +3,7 @@
 """
 from os import environ
 import psycopg2
+import psycopg2.pool
 from pathlib import Path
 from minio import Minio
 from minio.commonconfig import Tags
@@ -23,30 +24,42 @@ minio_policy = {
     "Statement": [
         {
             "Effect": "Deny",
-            "Action": ["s3:ForceDeleteBucket", "s3:DeleteBucket"],
-            "Resource": "arn:aws:s3:::*",
+            "Action": [
+                "s3:DeleteBucket",
+                "s3:ForceDeleteBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::*"
+            ]
         }
-    ],
-    }
+    ]
+}
 
+# FIXME make this auto detect if its running inside the cluster or not
 params = {
-    "host": "pg.berlinunited-cloud.de",
-    "port": 4000,
+    "host": "postgres-postgresql.default.svc",
+    "port": 5432,
     "dbname": "logs",
     "user": "naoth",
     "password": environ.get('DB_PASS')
 }
+#pool = psycopg2.pool.SimpleConnectionPool( 
+#    2, 3, user='naoth', password= environ.get('DB_PASS'), 
+#    host="pg.berlinunited-cloud.de", port='4000', database='logs')
 conn = psycopg2.connect(**params)
-cur = conn.cursor()
+cursor = conn.cursor()
 
 
 def get_logs():
     select_statement = f"""
     SELECT log_path FROM robot_logs WHERE extract_status = true 
     """
-    cur.execute(select_statement)
-    rtn_val = cur.fetchall()
+    #connection = pool.getconn()
+    #cursor = connection.cursor() 
+    cursor.execute(select_statement)
+    rtn_val = cursor.fetchall()
     logs = [x[0] for x in rtn_val]
+    #pool.putconn(connection) 
     return logs
 
 
@@ -57,9 +70,8 @@ def generate_unique_name():
             return name
 
 
-def upload_to_minio(data_folder):
+def upload_to_minio(bucket_name, data_folder):
     # TODO check if a name in db exist already and use that 
-    bucket_name = generate_unique_name()
 
     # Make the bucket
     # TODO maybe add a try here so that we can remove the bucket if something goes wrong
@@ -68,14 +80,14 @@ def upload_to_minio(data_folder):
     tags = Tags.new_bucket_tags()
     tags["data path"] = str(data_folder)
     mclient.set_bucket_tags(bucket_name, tags)
-    mclient.set_bucket_policy(bucket_name, json.dumps(minio_policy))
+    #mclient.set_bucket_policy(bucket_name, json.dumps(minio_policy))
     
-    print("Created bucket", bucket_name)
+    print("\t\tCreated bucket", bucket_name)
 
-    print(f"Upload files in {data_folder}")
+    print(f"\t\tUpload files in {data_folder}")
     files = Path(data_folder).glob("*")
     for file in files:
-        print(f"{file}", end="\r", flush=True)
+        print(f"\t\t{file.name}", end="\r", flush=True)
         source_file = file
         destination_file = Path(file).name
         mclient.fput_object(
@@ -83,7 +95,6 @@ def upload_to_minio(data_folder):
             destination_file,
             source_file,
         )
-    return bucket_name
 
 
 if __name__ == "__main__":
@@ -95,10 +106,12 @@ if __name__ == "__main__":
     log_list = get_logs()
 
     for log_folder in log_list:
+        print(log_folder)
         actual_log_folder = root_path / Path(log_folder)
         combined_log = root_path / Path(actual_log_folder) / "combined.log"
         game_log = root_path / Path(actual_log_folder) / "game.log"
 
+        # calculate the location of the folders for the images 
         extracted_folder = (
             Path(actual_log_folder).parent.parent
             / Path("extracted")
@@ -112,19 +125,29 @@ if __name__ == "__main__":
         select_statement = f"""
         SELECT bucket_top FROM robot_logs WHERE log_path = '{log_folder}' 
         """
-        cur.execute(select_statement)
-        rtn_val = cur.fetchall()[0][0]
+        #connection = pool.getconn()
+        #cursor = connection.cursor()
+        cursor.execute(select_statement)
+        rtn_val = cursor.fetchall()[0][0]
+        #pool.putconn(connection) 
         if rtn_val is not None:
             # TODO this must be handled better for example what if minio goes down and looses all the data, I need to recreate it the same way. this way I can also use the labelstudio backup
-            print("bucket already exists")
+            print("\tbucket already exists")
         else:
-            bucket_name_top = upload_to_minio(data_folder_top)
+            bucket_name_top = generate_unique_name()
+            print(f"\tupload data for upper camera to bucket {bucket_name_top}")
             insert_statement = f"""
             UPDATE robot_logs SET bucket_top = '{bucket_name_top}' WHERE log_path = '{log_folder}';
             """
-            cur.execute(insert_statement)
+            #connection = pool.getconn()
+            #cursor = connection.cursor()
+            cursor.execute(insert_statement)
             conn.commit()
+            #pool.putconn(connection) 
 
+            upload_to_minio(bucket_name_top, data_folder_top)
+
+        continue
         # check if bucket for bottom data exists (FIXME make this code cooler)
         select_statement = f"""
         SELECT bucket_bottom FROM robot_logs WHERE log_path = '{log_folder}' 
@@ -132,11 +155,14 @@ if __name__ == "__main__":
         cur.execute(select_statement)
         rtn_val = cur.fetchall()[0][0]
         if rtn_val is not None:
-            print("bucket already exists")
+            print("\tbucket already exists")
         else:
-            bucket_name_bottom = upload_to_minio(data_folder_bottom)
+            bucket_name_bottom = generate_unique_name()
+            print(f"\tupload data for lower camera to bucket {bucket_name_bottom}")
             insert_statement = f"""
             UPDATE robot_logs SET bucket_bottom = '{bucket_name_bottom}' WHERE log_path = '{log_folder}';
             """
             cur.execute(insert_statement)
             conn.commit()
+            
+            upload_to_minio(bucket_name_bottom, data_folder_bottom)
