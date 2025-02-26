79f54b470370d8c1afaaef28d4639e770080256e
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-02-26 09:29:52 +0100

    fix stuff for image inserts

--- 12_input_images.py
+++ 12_input_images.py
@@ -23,26 +23,26 @@ def path_generator(directory: str, batch_size: int = 200) -> Generator[List[str]
     if batch:
         yield batch
 
-def handle_insertion(individual_extracted_folder, data, camera, type):
+def handle_insertion(individual_extracted_folder, log, camera, image_type):
 
     print(individual_extracted_folder)
     if not Path(individual_extracted_folder).is_dir():
         return
-    if is_done(log_id, camera, type):
+
+    if is_done(log.id, camera, image_type):
         return
 
     for batch in path_generator(individual_extracted_folder):
         image_ar = [None] * len(batch)
         for idx, file in enumerate(batch):
-
+            # get frame number
             framenumber = int(Path(file).stem)
             url_path = str(file).removeprefix(log_root_path).strip("/")
             
             image_ar[idx] = {
-                "log": log_id,
+                "frame": framenumber,
                 "camera": camera,
-                "type": type,
-                "frame_number": framenumber,
+                "type": image_type,
                 "image_url": url_path,
                 # HACK we need to provide some default values
                 "blurredness_value": None,
@@ -60,8 +60,8 @@ def handle_insertion(individual_extracted_folder, data, camera, type):
         sleep(0.5)
     #sleep(5)
 
-def is_done(robot_data_id, camera, type):
-    response = client.image.get_image_count(log_id=robot_data_id, camera=camera, type=type)
+def is_done(robot_data_id, camera, image_type):
+    response = client.image.get_image_count(log=robot_data_id, camera=camera, type=image_type)
     db_count = int(response["count"])
     # FIXME use the correct data for check if its done
     response2 = client.log_status.list(log=robot_data_id)
@@ -70,13 +70,13 @@ def is_done(robot_data_id, camera, type):
         return False
     log_status = response2[0]
 
-    if camera == "BOTTOM" and type == "RAW":
+    if camera == "BOTTOM" and image_type == "RAW":
         target_count = int(log_status.num_bottom)
-    elif camera == "TOP" and type == "RAW":
+    elif camera == "TOP" and image_type == "RAW":
         target_count = int(log_status.num_top)
-    elif camera == "BOTTOM" and type == "JPEG":
+    elif camera == "BOTTOM" and image_type == "JPEG":
         target_count = int(log_status.num_jpg_bottom)
-    elif camera == "TOP" and type == "JPEG":
+    elif camera == "TOP" and image_type == "JPEG":
         target_count = int(log_status.num_jpg_top)
     else:
         ValueError()
@@ -97,12 +97,12 @@ if __name__ == "__main__":
 
     existing_data = client.logs.list()
 
-    def myfunc(data):
-        return data.log_path
+    def myfunc(log):
+        return log.log_path
 
-    for data in sorted(existing_data, key=myfunc, reverse=True):
-        log_id = data.id
-        log_path = Path(log_root_path) / data.log_path
+    for log in sorted(existing_data, key=myfunc, reverse=True):
+        log_id = log.id
+        log_path = Path(log_root_path) / log.log_path
 
         # TODO could we just switch game_logs with extracted in the paths?
         robot_foldername = log_path.parent.name
@@ -114,7 +114,7 @@ if __name__ == "__main__":
         bottom_path_jpg = extracted_path / "log_bottom_jpg"
         top_path_jpg = extracted_path / "log_top_jpg"
 
-        handle_insertion(bottom_path, data, camera="BOTTOM", type="RAW")
-        handle_insertion(top_path, data, camera="TOP", type="RAW")
-        handle_insertion(bottom_path_jpg, data, camera="BOTTOM", type="JPEG")
-        handle_insertion(top_path_jpg, data, camera="TOP", type="JPEG")
+        handle_insertion(bottom_path, log, camera="BOTTOM", image_type="RAW")
+        handle_insertion(top_path, log, camera="TOP", image_type="RAW")
+        handle_insertion(bottom_path_jpg, log, camera="BOTTOM", image_type="JPEG")
+        handle_insertion(top_path_jpg, log, camera="TOP", image_type="JPEG")
