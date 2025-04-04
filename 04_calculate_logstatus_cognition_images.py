ec04010e452a4dc5986e2e2e9da82e8b2374f152
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-04-04 16:14:22 +0200

    use extra script for calculating images inside the combined log

---
+++ 04_calculate_logstatus_cognition_images.py
@@ -0,0 +1,127 @@
+from pathlib import Path
+from naoth.log import Reader as LogReader
+from naoth.log import Parser
+import os
+from google.protobuf.json_format import MessageToDict
+from vaapi.client import Vaapi
+from tqdm import tqdm
+import argparse
+
+
+def is_done(log_id, status_dict):
+    new_dict = status_dict.copy()
+    try:
+        # we use list here because we only know the log_id here and not the if of the logstatus object
+        response = client.log_status.list(log=log_id)
+        if len(response) == 0:
+            return status_dict
+        log_status = response[0]
+
+        invalid_data = False
+        for k, v in status_dict.items():
+            field_value = getattr(log_status, k)
+            #print(k, v, field_value)
+            if field_value == None:
+                print(f"\tdid not find a value for repr {k}")
+            elif field_value > log_status.FrameInfo:
+                invalid_data = True
+                print(f"\tfound value exceeding number of full frames for repr {k}")
+            else:
+                new_dict.pop(k)
+        # Include FrameInfo when we have representations that exceed the number of FrameInfos
+        # Covers the case that the number of FrameInfos was calculated wrongly
+        if invalid_data:
+            new_dict.update({"FrameInfo": 0})
+
+        return new_dict
+    # TODO would be nice to handle the vaapi API error here explicitely
+    except Exception as e:
+        print("error", e)
+        quit()
+        return status_dict
+
+
+def add_gamelog_representations(log, log_path):
+    # make a dictionary out of the representation names which can later be used to count
+    cognition_status_dict = {
+        "Image": 0,
+        "ImageTop": 0,
+        "ImageJPEG": 0,
+        "ImageJPEGTop": 0,
+    }
+    new_cognition_status_dict = is_done(log.id, cognition_status_dict)
+    if not args.force and len(new_cognition_status_dict) == 0:
+        print("\twe already calculated number of full cognition frames for this log")
+    else:
+        if args.force:
+            new_cognition_status_dict = cognition_status_dict
+
+        my_parser = Parser()
+        my_parser.register("ImageTop", "Image")
+        my_parser.register("ImageJPEG"   , "Image")
+        my_parser.register("ImageJPEGTop", "Image")
+
+        game_log = LogReader(str(log_path), my_parser)
+        # FIXME can we parse the game.log here instead of the combined log and then check the images.log and images.jpeg? That would be much faster
+        # when counting images from images_jpeg log we need to be careful to only count frames containing it
+        for idx, frame in enumerate(tqdm(game_log)):
+            # stop parsing log if FrameInfo is missing
+            try:
+                frame_number = frame['FrameInfo'].frameNumber
+            except Exception as e:
+                print(f"FrameInfo not found in current frame - will not parse any other frames from this log and continue with the next one")
+                break
+
+            for repr in new_cognition_status_dict:
+                try:
+                    data = MessageToDict(frame[repr])
+                    new_cognition_status_dict[repr] += 1
+                except AttributeError:
+                    # TODO only print something when in debug mode
+                    #print("skip frame because representation is not present")
+                    continue
+                except KeyError:
+                    # if image is not in frame
+                    continue
+                except Exception as e:
+                    print(f"error parsing {repr} in log {log_path} at frame {idx}")
+                    print({e})
+
+        try:
+            response = client.log_status.update(
+                log=log.id, 
+                **new_cognition_status_dict
+            )
+        except Exception as e:
+            print(f"\terror inputing the data {log_path}")
+            print(e)
+            quit()
+
+
+def main():
+    log_root_path = os.environ.get("VAT_LOG_ROOT")
+    
+    existing_data = client.logs.list()
+
+    def sort_key_fn(data):
+        return data.log_path
+
+    for log in sorted(existing_data, key=sort_key_fn, reverse=True):
+        # TODO use combined log if its a file. -> it should always be a file if not experiment
+        log_path = Path(log_root_path) / log.combined_log_path
+
+        print(f"{log.id}: {log_path}")
+        add_gamelog_representations(log, log_path)
+        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--force", action="store_true", default=False)
+    args = parser.parse_args()
+
+    client = Vaapi(
+        base_url=os.environ.get("VAT_API_URL"),
+        api_key=os.environ.get("VAT_API_TOKEN"),
+    )
+
+    main()
